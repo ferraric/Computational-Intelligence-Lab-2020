133192770c8e3950e254f5211abddc0157cf2db1
Sinan Demirci <demircis@student.ethz.ch>
2020-04-25 12:12:09 +0200

    -added validation split -store predicitons in numpy file -refactoring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from dataloader import DataLoader
 from BERT import BERT
-from sklearn.preprocessing import LabelBinarizer
 import pandas as pd
 import numpy as np
 import tensorflow as tf
@@ -15,18 +14,20 @@ def main():
     tokenizer = bert.create_tokenizer_from_module()
 
     inputs = bert.transform_sentences(train_df['sentence'].to_numpy(), tokenizer)
+    one_hot_labels = tf.keras.utils.to_categorical(train_df['polarity'].to_numpy(), num_classes=2)
 
-    model = bert.create_fc_model(dropout_rate=0.2)
+    model = bert.create_fc_model(dropout_rate=0.1)
 
     model.compile(optimizer=tf.keras.optimizers.Adam(lr=3e-5), loss='binary_crossentropy', metrics=['accuracy'])
 
     print(model.summary())
 
-    model.fit(x=inputs, y=tf.keras.utils.to_categorical(train_df['polarity'].to_numpy(), num_classes=2), epochs=2, batch_size=32)
+    model.fit(x=inputs, y=one_hot_labels, epochs=3, batch_size=32, validation_split=0.2)
 
     test_inputs = bert.transform_sentences(test_df['sentence'].to_numpy(), tokenizer)
 
     predicted_labels = model.predict(x=test_inputs)
+    np.save('./outputs/results.npy', predicted_labels, allow_pickle=True)
 
     result = pd.DataFrame(data=np.argmax(predicted_labels, axis=1))
     result.index += 1
