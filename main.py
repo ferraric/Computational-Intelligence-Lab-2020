740991e170c497b6868e84314de40390d029fff0
Sinan Demirci <demircis@student.ethz.ch>
2020-04-28 16:14:51 +0200

    fix bug in preprocessing

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ def main():
     train_df['sentence'] = train_df['sentence'].apply(lambda text: "".join([c for c in text if c not in string.punctuation]))
     test_df['sentence'] = test_df['sentence'].apply(lambda text: "".join([c for c in text if c not in string.punctuation]))
 
-    train_df['sentence'] = train_df['sentence'].apply(lambda text: text.replace('<user>').replace('<url>'))
-    test_df['sentence'] = test_df['sentence'].apply(lambda text: text.replace('<user>').repace('<url>'))
+    train_df['sentence'] = train_df['sentence'].apply(lambda text: text.replace('<user>', '').replace('<url>', ''))
+    test_df['sentence'] = test_df['sentence'].apply(lambda text: text.replace('<user>', '').repace('<url>', ''))
     bert = BERT(bert_model_size='albert_base')
 
     tokenizer = bert.create_tokenizer_from_module()
     inputs = bert.transform_sentences(train_df['sentence'].to_numpy(), tokenizer)
     one_hot_labels = tf.keras.utils.to_categorical(train_df['polarity'].to_numpy(), num_classes=2)
 
-    model = bert.create_fc_model(dropout_rate=0.1)
+    model = bert.create_fc_model(dropout_rate=0.2)
 
     model.compile(optimizer=tf.keras.optimizers.Adam(lr=3e-5), loss='binary_crossentropy', metrics=['accuracy'])
 
