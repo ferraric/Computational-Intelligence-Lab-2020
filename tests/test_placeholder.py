752b55b44867cb93e72851b4593357565b3ac7d0
Claudio Ferrari <ferraric@ethz.ch>
2020-06-04 12:05:57 +0200

    added placeholder test to please lord travis

---
+++ tests/test_placeholder.py
@@ -0,0 +1,11 @@
+"""
+Our continuous integration pipeline ensures that pull requests can only be
+merged to master if all builds pass. If there are no tests, by default the build fails.
+This is a placeholder test in order to make the build pass. It can and should be
+removed as soon as we have a first meaningful test.
+TODO: remove this once we have another test
+"""
+
+
+def test_placeholder():
+    pass
