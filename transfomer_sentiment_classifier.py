e6d364236defca5f880d7d4a6b74418315a563ed
Jérémy Scheurer <jeremyalain.scheurer@gmail.com>
2020-06-04 22:32:37 +0200

    initial Bert classifier from tutorial, all in one file. also removed … (#9)
    * initial Bert classifier from tutorial, all in one file. also removed flake8 test
    
    * added flake 8 back to pre-commit-config and added .flake8 config file to make it work with black
    
    * Simple skeleton code
    
    * addressed first MR on utilities file
    
    * addressed MR review 2
    
    * addressed MR discission 3
    
    * addressed MR discussion 4

---
+++ transfomer_sentiment_classifier.py
@@ -0,0 +1,90 @@
+import os
+import sys
+import inspect
+from comet_ml import Experiment
+import logging
+import torch
+import transformers
+import pytorch_lightning as pl
+from utilities.general_utilities import get_args, get_bunch_config_from_json
+from bunch import Bunch
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+
+class TransformerSentimentClassifier(pl.LightningModule):
+    def __init__(self, config: Bunch) -> None:
+        super().__init__()
+        self.config = config
+        self.model = transformers.BertForSequenceClassification.from_pretrained(
+            config.transformer_model
+        )
+
+    def prepare_data(self):
+        pass
+
+    def forward(self, input_ids):
+        pass
+
+    def training_step(self, batch, batch_idx):
+        pass
+
+    def validation_step(self, batch, batch_idx):
+        pass
+
+    def validation_epoch_end(self, outputs):
+        pass
+
+    def train_dataloader(self):
+        pass
+
+    def validation_dataloader(self):
+        pass
+
+    def test_data_loader(self):
+        pass
+
+    def configure_optimizers(self):
+        pass
+
+
+def setup_comet_logger(config: Bunch) -> Experiment:
+    assert config.comet_api_key is not None, "Comet api key not defined in config"
+    assert (
+        config.comet_project_name is not None
+    ), "Comet project name not defined in config"
+    assert config.comet_workspace is not None, "Comet workspace not defined in config"
+    assert (
+        config.use_comet_experiments is not None
+    ), "Comet use_experiment flag not defined in config"
+    assert (
+        config.experiment_name is not None
+    ), "Comet experiment name is not defined in config"
+
+    comet_experiment = Experiment(
+        api_key=config.comet_api_key,
+        project_name=config.comet_project_name,
+        workspace=config.comet_workspace,
+        disabled=not config.use_comet_experiments,
+    )
+    return comet_experiment
+
+
+def main():
+    try:
+        args = get_args()
+        config = get_bunch_config_from_json(args.config)
+    except RuntimeError:
+        logging.exception(
+            "You need to pass a config as argument, i.e. pass -c /path/to/config_file.json"
+        )
+        exit(0)
+    comet_experiment = setup_comet_logger(config)
+    comet_experiment.log_parameters(config)
+    # use_gpus = 1 if torch.cuda.is_available() else 0
+
+
+if __name__ == "__main__":
+    main()
---
+++ utilities/general_utilities.py
@@ -0,0 +1,24 @@
+import argparse
+import json
+from bunch import Bunch
+
+
+def get_args() -> argparse.Namespace:
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        "-c",
+        "--config",
+        help="Add the Configuration file that has all the relevant parameters",
+    )
+    return argparser.parse_args()
+
+
+def get_bunch_config_from_json(json_file_path: str) -> Bunch:
+    """
+    Get the config from a json file and save it as a Bunch object.
+    :param json_file:
+    :return: config as Bunch object:
+    """
+    with open(json_file_path, "r") as config_file:
+        config_dict = json.load(config_file)
+    return Bunch(config_dict)
