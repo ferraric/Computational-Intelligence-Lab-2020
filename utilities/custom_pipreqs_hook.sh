5aa7792ee090a6dffdc5899de2968d3c78876f23
Claudio Ferrari <ferraric@ethz.ch>
2020-06-10 15:29:47 +0200

    automatic requirements generation

---
+++ utilities/custom_pipreqs_hook.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+pipreqs --use-local --ignore pipreqs_ignored --savepath utilities/pipreqs.txt
+sort utilities/non_imported_requirements.txt utilities/pipreqs.txt | uniq  > requirements.txt
+rm utilities/pipreqs.txt
