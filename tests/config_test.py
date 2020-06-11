efcfee14958efe65aeacacf5a1b8f15bd05c12de
Claudio Ferrari <ferraric@ethz.ch>
2020-06-11 14:49:39 +0200

    check that strings are read correctly

--- tests/config_test.py
+++ tests/config_test.py
@@ -8,3 +8,6 @@ TEST_CONFIG_FILE = "example_config.json"
 class ConfigTest(TestCase):
     def setUp(self) -> None:
         self.config = get_bunch_config_from_json(TEST_CONFIG_FILE)
+
+    def test_string_config_value(self) -> None:
+        assert self.config.string_config_key == "some string"
