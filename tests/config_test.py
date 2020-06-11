6d201f31837442c33f68f22a9b4141b8f22da006
Claudio Ferrari <ferraric@ethz.ch>
2020-06-11 14:47:55 +0200

    test setup and empty example config

---
+++ tests/config_test.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+from utilities.general_utilities import get_bunch_config_from_json
+
+TEST_CONFIG_FILE = "example_config.json"
+
+
+class ConfigTest(TestCase):
+    def setUp(self) -> None:
+        self.config = get_bunch_config_from_json(TEST_CONFIG_FILE)
