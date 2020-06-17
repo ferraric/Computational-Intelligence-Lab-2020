e5a6615f2f9f854732feea8585d77e4a857b722e
Claudio Ferrari <ferraric@ethz.ch>
2020-06-17 14:55:30 +0200

    load and store train and validation data

--- transfomer_sentiment_classifier.py
+++ transfomer_sentiment_classifier.py
@@ -2,10 +2,13 @@ import inspect
 import os
 import sys
 
+import numpy as np
 import pytorch_lightning as pl
+import torch
 import transformers
 from bunch import Bunch
 from comet_ml import Experiment
+from torch.utils.data import TensorDataset, random_split
 from utilities.general_utilities import get_args, get_bunch_config_from_json
 
 currentdir = os.path.dirname(
@@ -23,7 +26,35 @@ class TransformerSentimentClassifier(pl.LightningModule):
         )
 
     def prepare_data(self) -> None:
-        pass
+        tokenizer = transformers.BertTokenizer.from_pretrained(
+            self.config.transformer_model
+        )
+
+        with open(self.config.negative_tweets_path) as f:
+            text_lines_neg = f.read().splitlines()
+        with open(self.config.positive_tweets_path) as f:
+            text_lines_pos = f.read().splitlines()
+        all_tweets = text_lines_neg + text_lines_pos
+        labels = np.concatenate(
+            (np.zeros(len(text_lines_neg)), np.ones(len(text_lines_pos)))
+        )
+
+        tokenized_input = tokenizer.batch_encode_plus(
+            all_tweets,
+            max_length=self.config.max_tokens_per_tweet,
+            pad_to_max_length=True,
+            return_token_type_ids=False,
+        )
+        input_ids = torch.tensor(tokenized_input["input_ids"], dtype=int)
+        attention_mask = torch.tensor(tokenized_input["attention_mask"], dtype=int)
+        labels = torch.tensor(labels, dtype=int)
+        all_data = TensorDataset(input_ids, attention_mask, labels)
+
+        n_val_samples = int(0.1 * len(all_data))
+        n_train_samples = len(all_data) - n_val_samples
+        self.train_data, self.val_data = random_split(
+            all_data, [n_train_samples, n_val_samples]
+        )
 
     def forward(self, input_ids: None) -> None:
         pass
