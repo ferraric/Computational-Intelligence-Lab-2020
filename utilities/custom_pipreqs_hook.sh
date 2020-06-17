fc34966232bb6af792bb879fdd6aa66f028ebcd4
Claudio Ferrari <ferraric@ethz.ch>
2020-06-17 20:51:26 +0200

    travis debugging

--- utilities/custom_pipreqs_hook.sh
+++ utilities/custom_pipreqs_hook.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 pipreqs --use-local --ignore pipreqs_ignored --savepath utilities/pipreqs.txt
 sort utilities/non_imported_requirements.txt utilities/pipreqs.txt | uniq  > requirements.txt
+cat requirements.txt
 rm utilities/pipreqs.txt
