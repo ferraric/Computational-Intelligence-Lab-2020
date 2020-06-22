695855f4786cd719c31cb77e5492b0373b78b804
Sinan Demirci <demirci.sinan96@gmail.com>
2020-06-22 19:40:07 +0200

    add .gitattributes and update pipreqs script to handle line separators (windows) and update .gitignore

--- utilities/custom_pipreqs_hook.sh
+++ utilities/custom_pipreqs_hook.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 pipreqs --use-local --ignore pipreqs_ignored,.mypy_cache --savepath utilities/pipreqs.txt
-sort utilities/non_imported_requirements.txt utilities/pipreqs.txt | uniq > requirements.txt
+sort utilities/non_imported_requirements.txt utilities/pipreqs.txt | uniq > temp_requirements.txt
+tr -d '\015' < temp_requirements.txt > requirements.txt
 rm utilities/pipreqs.txt
+rm temp_requirements.txt
