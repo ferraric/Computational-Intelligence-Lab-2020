18d5482e445f9177e90cb8391dd10c79ab1d2590
Sinan Demirci <demirci.sinan96@gmail.com>
2020-06-24 09:14:05 +0200

    revert change in pipreqs script, fix requirements files to LF endings

--- utilities/custom_pipreqs_hook.sh
+++ utilities/custom_pipreqs_hook.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 pipreqs --use-local --ignore pipreqs_ignored,.mypy_cache --savepath utilities/pipreqs.txt
-sort utilities/non_imported_requirements.txt utilities/pipreqs.txt | uniq > temp_requirements.txt
-tr -d '\015' < temp_requirements.txt > requirements.txt
+sort utilities/non_imported_requirements.txt utilities/pipreqs.txt | uniq > requirements.txt
 rm utilities/pipreqs.txt
-rm temp_requirements.txt
