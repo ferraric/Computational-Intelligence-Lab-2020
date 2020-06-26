d7d1c60e0e2fd73d574cbc6119a969bd07e4d653
Claudio Ferrari <ferraric@lo-login-01.leonhard.ethz.ch>
2020-06-26 10:34:22 +0200

    pre-commit on leonhard

--- init_leonhard.sh
+++ init_leonhard.sh
@@ -2,3 +2,15 @@ module load gcc/6.3.0 python_gpu/3.7.4
 module load eth_proxy
 source venv/bin/activate
 pip3 install -r requirements.txt
+pre-commit install
+# The pre-commit hook isort looks through all these config files.
+# If it does not find them in the project root folder,
+# it tries to look for them in the $HOME folder (/cluster/home),
+# where it does not have permission and thus fails.
+# We create emtpy config files in the project root folder
+# in order to prevent that.
+touch .editorconfig
+touch pyproject.toml
+touch .isort.cfg
+touch setup.cfg
+touch tox.ini
