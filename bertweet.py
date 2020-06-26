f7e7b80914eca3ddb557f516c3be1ea1c23fdf41
Claudio Ferrari <ferraric@ethz.ch>
2020-06-26 16:13:49 +0200

    bertweet data loading and tokenization

--- bertweet.py
+++ bertweet.py
@@ -1,9 +1,15 @@
 import os
+from argparse import Namespace
+from typing import List
 
 import pytorch_lightning as pl
+import torch
 from bert_sentiment_classifier import BertSentimentClassifier
 from bunch import Bunch
+from fairseq.data import Dictionary
+from fairseq.data.encoders.fastbpe import fastBPE
 from torch.nn import CrossEntropyLoss
+from torch.utils.data import TensorDataset
 from transformers import RobertaConfig, RobertaModel
 from transformers.modeling_roberta import RobertaClassificationHead
 
@@ -27,3 +33,73 @@ class BERTweet(BertSentimentClassifier):
         )
         self.classifier = RobertaClassificationHead(model_config)
         self.loss = CrossEntropyLoss()
+
+    def _load_tweets(self, path: str) -> List[str]:
+        def replace_special_tokens(tweet: str) -> str:
+            return tweet.replace("<url>", "HTTPURL").replace("<user>", "@USER")
+
+        with open(path, encoding="utf-8") as f:
+            return list(map(replace_special_tokens, f.read().splitlines()))
+
+    def prepare_data(self) -> None:
+        bpe_codes_path = os.path.join(
+            self.config.pretrained_model, "BERTweet_base_transformers/bpe.codes"
+        )
+        bpe = fastBPE(Namespace(bpe_codes=bpe_codes_path))
+        vocab = Dictionary()
+        vocab.add_from_file(
+            os.path.join(
+                self.config.pretrained_model, "BERTweet_base_transformers/dict.txt"
+            )
+        )
+
+        def _split_into_tokens(tweet: str) -> str:
+            return "<s> " + bpe.encode(tweet) + " <s>"
+
+        def _encode(token_string: str) -> List[int]:
+            return (
+                vocab.encode_line(
+                    token_string, append_eos=False, add_if_not_exist=False
+                )
+                .long()
+                .tolist()
+            )
+
+        negative_tweets = self._load_tweets(self.config.negative_tweets_path)
+        positive_tweets = self._load_tweets(self.config.positive_tweets_path)
+        all_tweets = negative_tweets + positive_tweets
+        labels = self._generate_labels(len(negative_tweets), len(positive_tweets))
+
+        token_strings = list(map(_split_into_tokens, all_tweets))
+        token_id_list = list(map(_encode, token_strings))
+
+        test_tweets = self._load_tweets(self.config.test_tweets_path)
+        token_strings = list(map(_split_into_tokens, test_tweets))
+        test_token_id_list = list(map(_encode, token_strings))
+
+        max_token_length = max(map(len, token_id_list + test_token_id_list))
+
+        pad_token_id = vocab.pad()
+        token_ids = torch.tensor(
+            [
+                token_ids_per_tweet
+                + [pad_token_id] * (max_token_length - len(token_ids_per_tweet))
+                for token_ids_per_tweet in token_id_list
+            ]
+        )
+        attention_mask = (token_ids != pad_token_id).float()
+
+        self.train_data, self.validation_data = self._train_validation_split(
+            self.config.validation_size,
+            TensorDataset(token_ids, attention_mask, labels),
+        )
+
+        test_token_ids = torch.tensor(
+            [
+                token_ids_per_tweet
+                + [pad_token_id] * (max_token_length - len(token_ids_per_tweet))
+                for token_ids_per_tweet in test_token_id_list
+            ]
+        )
+        test_attention_mask = (test_token_ids != pad_token_id).float()
+        self.test_data = TensorDataset(test_token_ids, test_attention_mask)
