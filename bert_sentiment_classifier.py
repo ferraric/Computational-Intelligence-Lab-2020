80b4b2590b2d46cc1c757b1ac62ad9948fb2c1c1
Claudio Ferrari <ferraric@ethz.ch>
2020-07-01 11:13:52 +0200

    corrected index removal

--- bert_sentiment_classifier.py
+++ bert_sentiment_classifier.py
@@ -1,3 +1,4 @@
+import re
 from typing import Dict, List, Tuple, Union
 
 import pytorch_lightning as pl
@@ -69,7 +70,18 @@ class BertSentimentClassifier(pl.LightningModule):
         )
 
         test_tweets = self._load_tweets(self.config.test_tweets_path)
-        test_tweets_index_removed = [tweet.split(",")[1] for tweet in test_tweets]
+
+        def _remove_index_from_test_tweet(tweet: str) -> str:
+            test_tweet_format = re.compile("^[0-9]*,([\\s\\S]*)")
+            match = test_tweet_format.match(tweet)
+            if match:
+                return match.group(1)
+            else:
+                raise ValueError("unexpected test data format")
+
+        test_tweets_index_removed = [
+            _remove_index_from_test_tweet(tweet) for tweet in test_tweets
+        ]
         test_token_ids, test_attention_mask = self._tokenize_tweets(
             tokenizer, test_tweets_index_removed
         )
