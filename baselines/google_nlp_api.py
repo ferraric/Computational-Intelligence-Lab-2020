c9f4cf81e454a41a644d28035fab68f41e33827d
Claudio Ferrari <ferraric@ethz.ch>
2020-07-02 16:14:18 +0200

    getting predictions

--- baselines/google_nlp_api.py
+++ baselines/google_nlp_api.py
@@ -1,4 +1,9 @@
+import numpy as np
 from comet_ml import Experiment
+from google.cloud import language
+from google.cloud.language import enums, types
+from google.protobuf.json_format import MessageToDict
+from utilities.data_loading import load_tweets, remove_indices_from_test_tweets
 from utilities.general_utilities import get_args, get_bunch_config_from_json
 
 
@@ -14,6 +19,27 @@ def main() -> None:
     )
     comet_experiment.set_name(config.comet_experiment_name)
     comet_experiment.log_parameters(config)
+
+    test_tweets = load_tweets(config.test_data_path)
+    test_tweets_index_removed = remove_indices_from_test_tweets(test_tweets)
+
+    client = language.LanguageServiceClient()
+    result = []
+    predictions = np.zeros(len(test_tweets_index_removed))
+
+    for i, tweet in enumerate(test_tweets_index_removed):
+        document = types.Document(
+            type=enums.Document.Type.PLAIN_TEXT, content=tweet, language="en"
+        )
+
+        response = client.analyze_sentiment(document=document)
+        response_dict = MessageToDict(response)
+        result.append(response_dict)
+
+        prediction_present = bool(response_dict["documentSentiment"])
+        if prediction_present:
+            # -1, 1 predictions
+            predictions[i] = 2 * response.document_sentiment.score - 1
 
 
 if __name__ == "__main__":
