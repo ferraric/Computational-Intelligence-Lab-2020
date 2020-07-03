524eefdb70cd1df9d8e22c34c540a1e3b5666afb
Claudio Ferrari <ferraric@ethz.ch>
2020-07-03 09:50:05 +0200

    use avg of last layer embeddings

---
+++ bert_pooled_classifier.py
@@ -0,0 +1,26 @@
+import pytorch_lightning as pl
+import torch
+from bert_sentiment_classifier import BertSentimentClassifier
+from bunch import Bunch
+from torch.nn import CrossEntropyLoss, Dropout, Linear
+from transformers import BertConfig, BertModel
+
+
+class BertPooledClassifier(BertSentimentClassifier):
+    def __init__(self, config: Bunch) -> None:
+        pl.LightningModule.__init__(self)
+        self.config = config
+        model_config = BertConfig.from_pretrained(config.pretrained_model)
+        self.bert = BertModel.from_pretrained(config.pretrained_model)
+        self.dropout = Dropout(model_config.hidden_dropout_prob)
+        self.classifier = Linear(model_config.hidden_size, model_config.num_labels)
+        self.loss = CrossEntropyLoss()
+
+    def forward(
+        self, token_ids: torch.Tensor, attention_mask: torch.Tensor
+    ) -> torch.Tensor:
+        outputs = self.bert(token_ids, attention_mask)
+        token_embeddings = outputs[0]
+        avg_token_embedding = torch.mean(token_embeddings, dim=1)
+        avg_token_embedding = self.dropout(avg_token_embedding)
+        return self.classifier(avg_token_embedding)
---
+++ main_bert_pooled.py
@@ -0,0 +1,56 @@
+import os
+from datetime import datetime
+
+import pytorch_lightning as pl
+import torch
+from bert_pooled_classifier import BertPooledClassifier
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import CometLogger
+from utilities.general_utilities import get_args, get_bunch_config_from_json
+
+
+def main() -> None:
+    args = get_args()
+    config = get_bunch_config_from_json(args.config)
+    pl.seed_everything(config.random_seed)
+    current_timestamp = datetime.now().strftime("%y-%m-%d_%H-%M-%S")
+    save_path = os.path.join(
+        config.model_save_directory, config.experiment_name, current_timestamp
+    )
+    os.makedirs(save_path)
+
+    logger = CometLogger(
+        save_dir=save_path,
+        workspace=config.comet_workspace,
+        project_name=config.comet_project_name,
+        api_key=config.comet_api_key if config.use_comet_experiments else None,
+        experiment_name=config.experiment_name,
+    )
+    logger.log_hyperparams(config)
+    logger.log_hyperparams({"model_checkpoint_path": save_path})
+
+    save_model_callback = ModelCheckpoint(
+        os.path.join(save_path, "{epoch}-{val_loss:.2f}"), monitor="val_loss"
+    )
+    number_of_gpus = 1 if torch.cuda.is_available() else 0
+    trainer = pl.Trainer(
+        checkpoint_callback=save_model_callback,
+        deterministic=True,
+        fast_dev_run=config.debug,
+        gpus=number_of_gpus,
+        logger=logger,
+        max_epochs=config.epochs,
+    )
+
+    if args.test_model_path is None:
+        model = BertPooledClassifier(config)
+        trainer.fit(model)
+    else:
+        model = BertPooledClassifier.load_from_checkpoint(
+            args.test_model_path, config=config
+        )
+    trainer.test(model=model)
+
+
+if __name__ == "__main__":
+    main()
