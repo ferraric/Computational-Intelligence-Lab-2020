edb13b446b04790416990e08ea141c6c4e9f4df7
demircis <demirci.sinan96@gmail.com>
2020-07-09 11:08:17 +0200

    remove main function and fix imports

--- bert_bagging_ensemble_classifier.py
+++ bert_bagging_ensemble_classifier.py
@@ -1,17 +1,10 @@
-import os
-from datetime import datetime
-
 import numpy as np
-import pytorch_lightning as pl
 import torch
 from bert_sentiment_classifier import BertSentimentClassifier
 from bunch import Bunch
 from numpy.random._generator import default_rng
-from pytorch_lightning.callbacks import ModelCheckpoint
-from pytorch_lightning.loggers import CometLogger
 from torch.utils.data import TensorDataset
 from transformers import BertTokenizerFast
-from utilities.general_utilities import get_args, get_bunch_config_from_json
 
 
 class BertBaggingEnsembleClassifier(BertSentimentClassifier):
@@ -49,46 +42,3 @@ class BertBaggingEnsembleClassifier(BertSentimentClassifier):
             tokenizer, test_tweets
         )
         self.test_data = TensorDataset(test_token_ids, test_attention_mask)
-
-
-def main() -> None:
-    args = get_args()
-    config = get_bunch_config_from_json(args.config)
-    pl.seed_everything(config.random_seed)
-
-    current_timestamp = datetime.now().strftime("%y-%m-%d_%H-%M-%S")
-    save_path = os.path.join(
-        config.model_save_directory, config.experiment_name, current_timestamp
-    )
-    os.makedirs(save_path)
-
-    logger = CometLogger(
-        save_dir=save_path,
-        workspace=config.comet_workspace,
-        project_name=config.comet_project_name,
-        api_key=config.comet_api_key if config.use_comet_experiments else None,
-        experiment_name=config.experiment_name,
-    )
-    logger.log_hyperparams(config)
-
-    save_model_callback = ModelCheckpoint(
-        os.path.join(save_path, "{epoch}-{val_loss:.2f}"), monitor="val_loss"
-    )
-    number_of_gpus = 1 if torch.cuda.is_available() else 0
-    trainer = pl.Trainer(
-        checkpoint_callback=save_model_callback,
-        deterministic=True,
-        fast_dev_run=config.debug,
-        gpus=number_of_gpus,
-        logger=logger,
-        max_epochs=config.epochs,
-    )
-
-    if args.test_model_path is None:
-        model = BertSentimentClassifier(config)
-        trainer.fit(model)
-    else:
-        model = BertSentimentClassifier.load_from_checkpoint(
-            args.test_model_path, config=config
-        )
-    trainer.test(model=model)
