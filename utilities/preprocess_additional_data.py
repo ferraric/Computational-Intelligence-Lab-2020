c37cf52c6fd5a2ce1a5a854d8727cfaf41bd5465
Claudio Ferrari <ferraric@ethz.ch>
2020-07-10 09:27:53 +0200

    path as args

--- utilities/preprocess_additional_data.py
+++ utilities/preprocess_additional_data.py
@@ -1,3 +1,4 @@
+import argparse
 import html
 import os
 import re
@@ -24,14 +25,18 @@ def preprocess(tweet: str) -> str:
 
 
 if __name__ == "__main__":
-    data_path = "../data"
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("-i", "--input_tweets", help="Add input tweets path")
+    argparser.add_argument(
+        "-o", "--output_folder", help="Save path for preprocessed tweets"
+    )
+    args = argparser.parse_args()
+
     url = re.compile(r"(?:(http[s]?://\S+)|((//)?(\w+\.)?\w+\.\w+/\S+))")
     user_mention = re.compile(r"(?:(?<!\w)@\w+\b)")
 
-    raw_data_path = os.path.join(data_path, "training.1600000.processed.noemoticon.csv")
-
     raw_data = pd.read_csv(  # type: ignore
-        raw_data_path,
+        args.input_tweets,
         encoding="latin-1",
         names=["label", "id", "date", "attr", "user", "tweet"],
     )
@@ -52,10 +57,10 @@ if __name__ == "__main__":
         if all(c in string.printable for c in tweet)
     ]
 
-    with open(os.path.join(data_path, "additional_train_neg.txt"), "w") as out:
+    with open(os.path.join(args.output_folder, "additional_train_neg.txt"), "w") as out:
         for neg_tweet in preprocessed_negative_tweets:
             out.write(neg_tweet + "\n")
 
-    with open(os.path.join(data_path, "additional_train_pos.txt"), "w") as out:
+    with open(os.path.join(args.output_folder, "additional_train_pos.txt"), "w") as out:
         for positive_tweet in preprocessed_positive_tweets:
             out.write(positive_tweet + "\n")
