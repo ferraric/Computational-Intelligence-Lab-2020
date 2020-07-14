b8d2c16e6df3db05e97f34172767799f3a9cca6e
Claudio Ferrari <ferraric@lo-login-02.leonhard.ethz.ch>
2020-07-14 17:09:30 +0200

    set pythonpath

--- init_leonhard.sh
+++ init_leonhard.sh
@@ -3,6 +3,7 @@ module load eth_proxy
 source venv/bin/activate
 pip3 install -r requirements.txt
 pre-commit install
+export PYTHONPATH=~/Computational-Intelligence-Lab-2020
 # to prevent the pre-commit hook "isort" to look through all config files in the $HOME folder 
 # (where it doesn't have permission and fails), we create empty config files in the project root folder
 touch .editorconfig
