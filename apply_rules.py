62afa4a77ba4a8bef135edb5191b2e153de6ffaf
Vanessa Tschichold <vanessa@tschichold.com>
2020-07-14 12:01:35 +0200

    print scores

--- apply_rules.py
+++ apply_rules.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple
 
+from sklearn.metrics import accuracy_score, confusion_matrix
 from utilities.data_loading import load_tweets, remove_indices_from_test_tweets
 
 
@@ -38,7 +39,25 @@ def get_rule_based_labels(
             predicted_labels_rule_based.append(x)
             bert_labels_rule_based.append(y)
             true_labels_rule_based.append(z)
+            print(predicted_labels_rule_based)
     return predicted_labels_rule_based, bert_labels_rule_based, true_labels_rule_based
+
+
+def get_scores(
+    count: int,
+    unknown_count: int,
+    predicted_labels: List[int],
+    bert_labels: List[int],
+    true_labels: List[int],
+) -> None:
+    print("nr of predictions: ", count)
+    print("nr of unknowns: ", unknown_count)
+    accuracy_rules = accuracy_score(true_labels, predicted_labels)
+    print("accuracy rules: ", accuracy_rules)
+    accuracy_bert = accuracy_score(true_labels, bert_labels)
+    print("accuracy bert: ", accuracy_bert)
+    confusion_matrix_rules = confusion_matrix(true_labels, predicted_labels)
+    print("confusion matrix rules: ", confusion_matrix_rules)
 
 
 def main() -> None:
@@ -49,9 +68,8 @@ def main() -> None:
     with open("data/rules/validation_labels.txt") as f:
         true_labels = [int(x) for x in f]
 
-    bert_labels = (
-        true_labels  # this are gonna be the real predictions of bert once finished
-    )
+    # this are gonna be the real predictions of bert once finished
+    bert_labels = true_labels
 
     predicted_labels, pos_count, neg_count, unknown_count = count_parentheses(
         tweets_index_removed
@@ -62,6 +80,14 @@ def main() -> None:
         bert_labels_rule_based,
         true_labels_rule_based,
     ) = get_rule_based_labels(predicted_labels, bert_labels, true_labels)
+
+    get_scores(
+        pos_count + neg_count,
+        unknown_count,
+        predicted_labels_rule_based,
+        bert_labels_rule_based,
+        true_labels_rule_based,
+    )
 
 
 if __name__ == "__main__":
