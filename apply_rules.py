dae30724e23856223c76babb719391953d17eb8d
Vanessa Tschichold <vanessa@tschichold.com>
2020-07-14 13:45:44 +0200

    fix parentheses

--- apply_rules.py
+++ apply_rules.py
@@ -12,8 +12,8 @@ def count_parentheses(data: List[str]) -> Tuple[List[int], int, int, int]:
     for tweet in data:
         pos = 0
         neg = 0
-        pos += tweet.count(" ( ")
-        neg += tweet.count(" ) ")
+        pos += tweet.count(" ) ")
+        neg += tweet.count(" ( ")
 
         if pos > neg:
             predicted_labels.append(1)
@@ -39,25 +39,29 @@ def get_rule_based_labels(
             predicted_labels_rule_based.append(x)
             bert_labels_rule_based.append(y)
             true_labels_rule_based.append(z)
-            print(predicted_labels_rule_based)
     return predicted_labels_rule_based, bert_labels_rule_based, true_labels_rule_based
 
 
-def get_scores(
-    count: int,
-    unknown_count: int,
-    predicted_labels: List[int],
-    bert_labels: List[int],
-    true_labels: List[int],
+def get_all_scores(
+    count: int, unknown_count: int, predicted_labels: List[int], true_labels: List[int],
 ) -> None:
+    print("-- all tweets --")
     print("nr of predictions: ", count)
-    print("nr of unknowns: ", unknown_count)
+    print("nr of unknown: ", unknown_count)
+    confusion_matrix_rules = confusion_matrix(true_labels, predicted_labels)
+    print("confusion matrix: ", confusion_matrix_rules)
+
+
+def get_rule_scores(
+    predicted_labels: List[int], bert_labels: List[int], true_labels: List[int],
+) -> None:
+    print("-- only tweets predicted with rules --")
     accuracy_rules = accuracy_score(true_labels, predicted_labels)
     print("accuracy rules: ", accuracy_rules)
     accuracy_bert = accuracy_score(true_labels, bert_labels)
     print("accuracy bert: ", accuracy_bert)
     confusion_matrix_rules = confusion_matrix(true_labels, predicted_labels)
-    print("confusion matrix rules: ", confusion_matrix_rules)
+    print("confusion matrix: ", confusion_matrix_rules)
 
 
 def main() -> None:
@@ -81,12 +85,12 @@ def main() -> None:
         true_labels_rule_based,
     ) = get_rule_based_labels(predicted_labels, bert_labels, true_labels)
 
-    get_scores(
-        pos_count + neg_count,
-        unknown_count,
-        predicted_labels_rule_based,
-        bert_labels_rule_based,
-        true_labels_rule_based,
+    get_all_scores(
+        pos_count + neg_count, unknown_count, predicted_labels, true_labels,
+    )
+
+    get_rule_scores(
+        predicted_labels_rule_based, bert_labels_rule_based, true_labels_rule_based,
     )
 
 
