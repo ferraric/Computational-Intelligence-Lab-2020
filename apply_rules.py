f802bd82e1d0ed697848ca2c6aae1dcdfe5e98fd
Vanessa Tschichold <vanessa@tschichold.com>
2020-07-14 16:17:18 +0200

    correct parenthesis function - draft

--- apply_rules.py
+++ apply_rules.py
@@ -4,18 +4,48 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 from utilities.data_loading import load_tweets, remove_indices_from_test_tweets
 
 
+def matched(tweet: str) -> bool:
+    diffCounter = 0
+    for i in tweet:
+        if i == "(":
+            diffCounter += 1
+        elif i == ")":
+            diffCounter -= 1
+    if diffCounter == 0:
+        return True
+    else:
+        return False
+
+
+# (() denn positiv
+# )(( denn unknown
+
+
 def count_parenthesis(data: List[str]) -> List[int]:
     rule_predictions = []
     for tweet in data:
+
         pos = 0
         neg = 0
         pos += tweet.count(" ) ")
         neg += tweet.count(" ( ")
 
         if pos > neg:
-            rule_predictions.append(1)
+            if neg != 0:
+                if matched(tweet):
+                    rule_predictions.append(1)
+                else:
+                    rule_predictions.append(0)
+            else:
+                rule_predictions.append(1)
         elif neg > pos:
-            rule_predictions.append(-1)
+            if pos != 0:
+                if matched(tweet):
+                    rule_predictions.append(-1)
+                else:
+                    rule_predictions.append(0)
+            else:
+                rule_predictions.append(-1)
         else:
             rule_predictions.append(0)
 
@@ -42,16 +72,16 @@ def get_subsets_rule_based(
 
 def print_all_scores(rule_predictions: List[int], labels: List[int]) -> None:
     print("-- all tweets --")
+    print("nr of rule_predictions: ", len([x for x in rule_predictions if x != 0]))
+    print("nr of unknown: ", len([x for x in rule_predictions if x == 0]))
     confusion_matrix_rules = confusion_matrix(labels, rule_predictions)
-    print("confusion matrix: ", confusion_matrix_rules)
+    print("confusion matrix:\n", confusion_matrix_rules)
 
 
 def print_rule_scores(
     rule_predictions: List[int], bert_predictions: List[int], labels: List[int],
 ) -> None:
     print("-- only tweets predicted with rules --")
-    print("nr of rule_predictions: ", len([x for x in rule_predictions if x != 0]))
-    print("nr of unknown: ", len([x for x in rule_predictions if x == 0]))
     accuracy_rules = accuracy_score(labels, rule_predictions)
     print("accuracy rules: ", accuracy_rules)
     accuracy_bert = accuracy_score(labels, bert_predictions)
@@ -63,7 +93,6 @@ def print_rule_scores(
 
 
 def main() -> None:
-
     tweets = load_tweets("data/rules/validation_data.txt")
     tweets_index_removed = remove_indices_from_test_tweets(tweets)
 
