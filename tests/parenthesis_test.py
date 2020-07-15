d1e5a92ded80ac8fa602592739d0a9fb70eba813
Vanessa Tschichold <vanessa@tschichold.com>
2020-07-15 16:58:57 +0200

    update test path

--- tests/parenthesis_test.py
+++ tests/parenthesis_test.py
@@ -1,41 +1,43 @@
 from unittest import TestCase
 
-from rules.apply_rules import remove_matching_parenthesis
+from rules.rules import RuleClassifier
 
 
 class ParenthesisTest(TestCase):
     def test_single_parenthesis_open(self) -> None:
-        assert remove_matching_parenthesis("(") == "("
+        assert RuleClassifier._remove_matching_parenthesis("(") == "("
 
     def test_single_parenthesis_closed(self) -> None:
-        assert remove_matching_parenthesis(")") == ")"
+        assert RuleClassifier._remove_matching_parenthesis(")") == ")"
 
     def test_two_parenthesis_closed(self) -> None:
-        assert remove_matching_parenthesis("()") == ""
+        assert RuleClassifier._remove_matching_parenthesis("()") == ""
 
     def test_two_parenthesis_nomatch(self) -> None:
-        assert remove_matching_parenthesis(")(") == ")("
+        assert RuleClassifier._remove_matching_parenthesis(")(") == ")("
 
     def test_two_parenthesis_open(self) -> None:
-        assert remove_matching_parenthesis("))") == "))"
+        assert RuleClassifier._remove_matching_parenthesis("))") == "))"
 
     def test_four_parenthesis_closed(self) -> None:
-        assert remove_matching_parenthesis("(())") == ""
+        assert RuleClassifier._remove_matching_parenthesis("(())") == ""
 
     def test_three_parenthesis_closed(self) -> None:
-        assert remove_matching_parenthesis("())") == ")"
+        assert RuleClassifier._remove_matching_parenthesis("())") == ")"
 
     def test_three_parenthesis_open(self) -> None:
-        assert remove_matching_parenthesis("(()") == "("
+        assert RuleClassifier._remove_matching_parenthesis("(()") == "("
 
     def test_three_parenthesis_nomatch(self) -> None:
-        assert remove_matching_parenthesis(")((") == ")(("
+        assert RuleClassifier._remove_matching_parenthesis(")((") == ")(("
 
     def test_four_parenthesis_nomatch(self) -> None:
-        assert remove_matching_parenthesis("))((") == "))(("
+        assert RuleClassifier._remove_matching_parenthesis("))((") == "))(("
 
     def test_four_parenthesis_twomatch(self) -> None:
-        assert remove_matching_parenthesis("()()") == ""
+        assert RuleClassifier._remove_matching_parenthesis("()()") == ""
 
     def test_three_parenthesis_test(self) -> None:
-        assert remove_matching_parenthesis("( hello ) ( ") == " hello  ( "
+        assert (
+            RuleClassifier._remove_matching_parenthesis("( hello ) ( ") == " hello  ( "
+        )
