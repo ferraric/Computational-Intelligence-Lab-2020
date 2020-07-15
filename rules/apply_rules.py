269fcd9a697264b0e3c1a4dc058c0c9c37331634
Claudio Ferrari <ferraric@ethz.ch>
2020-07-15 15:48:03 +0200

    refactored rules in other file

--- rules/apply_rules.py
+++ rules/apply_rules.py
@@ -1,59 +1,10 @@
-from collections import deque
 from typing import Callable, List, Sequence
 
 import numpy as np
 import pandas as pd
+from rules.rules import get_all_rules
 from sklearn.metrics import accuracy_score, confusion_matrix
 from utilities.data_loading import load_tweets, remove_indices_from_test_tweets
-
-
-def _remove_chars_at(indices: List[int], string: str) -> str:
-    char_array = np.array(list(string))
-    trimmed_char_array = np.delete(char_array, indices)
-    trimmed_string = "".join(trimmed_char_array)
-    return trimmed_string
-
-
-def remove_matching_parenthesis(tweet: str) -> str:
-    stack: deque = deque()
-    matching_indices = []
-
-    for index, char in enumerate(tweet):
-        if char == "(":
-            stack.append(index)
-        if char == ")":
-            if len(stack) > 0:
-                opening_index = stack.pop()
-                matching_indices.append(opening_index)
-                matching_indices.append(index)
-    return _remove_chars_at(matching_indices, tweet)
-
-
-def parenthesis_rule(tweet: str) -> int:
-    return classify(remove_matching_parenthesis(tweet), ")", "(")
-
-
-def heart_rule_no_space(tweet: str) -> int:
-    return classify(tweet, "<3", "</3")
-
-
-def heart_rule_space(tweet: str) -> int:
-    return classify(tweet, "< 3", "< / 3")
-
-
-def long_eyed_smiley_rule(tweet: str) -> int:
-    return classify(tweet, "=)", "=(")
-
-
-def classify(tweet: str, positive_pattern: str, negative_pattern: str) -> int:
-    if (positive_pattern in tweet) and (negative_pattern in tweet):
-        return 0
-    elif positive_pattern in tweet:
-        return 1
-    elif negative_pattern in tweet:
-        return -1
-    else:
-        return 0
 
 
 def apply_rules(tweet: str, rules: Sequence[Callable[[str], int]]) -> List[int]:
@@ -94,12 +45,7 @@ def main() -> None:
     # this are gonna be the real predictions of bert once finished
     bert_predictions = labels
 
-    rules = [
-        parenthesis_rule,
-        heart_rule_no_space,
-        heart_rule_space,
-        long_eyed_smiley_rule,
-    ]
+    rules = get_all_rules()
     rule_predictions = predict(tweets_index_removed, rules)
 
     print_confusion_matrix(
