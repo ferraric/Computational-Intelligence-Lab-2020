ee6aeaeb6f3c7dce46ea2a51f6ec74de2f96e7a5
Vanessa Tschichold <vanessa@tschichold.com>
2020-07-15 11:34:51 +0200

    clean parenthesis function

---
+++ rules/apply_rules.py
@@ -0,0 +1,126 @@
+from collections import deque
+from typing import List, Tuple
+
+import numpy as np
+from sklearn.metrics import accuracy_score, confusion_matrix
+from utilities.data_loading import load_tweets, remove_indices_from_test_tweets
+
+
+def _remove_chars_at(indices: List[int], string: str) -> str:
+    char_array = np.array(list(string))
+    trimmed_char_array = np.delete(char_array, indices)
+    trimmed_string = "".join(trimmed_char_array)
+    return trimmed_string
+
+
+def remove_matching_parenthesis(tweet: str) -> str:
+    stack: deque = deque()
+    matching_indices = []
+
+    for index, char in enumerate(tweet):
+        if char == "(":
+            stack.append(index)
+        if char == ")":
+            if len(stack) > 0:
+                opening_index = stack.pop()
+                matching_indices.append(opening_index)
+                matching_indices.append(index)
+    return _remove_chars_at(matching_indices, tweet)
+
+
+def classify(tweet: str) -> int:
+    if "(" in tweet:
+        if ")" in tweet:
+            return 0
+        else:
+            return -1
+    elif ")" in tweet:
+        if "(" in tweet:
+            return 0
+        else:
+            return 1
+    else:
+        return 0
+
+
+def count_parenthesis(tweets: List[str]) -> List[int]:
+    tweets_no_matching_parenthesis = [
+        remove_matching_parenthesis(tweet) for tweet in tweets
+    ]
+    return [classify(tweet) for tweet in tweets_no_matching_parenthesis]
+
+
+def get_subsets_rule_based(
+    rule_predictions: List[int], bert_predictions: List[int], labels: List[int]
+) -> Tuple[List[int], List[int], List[int]]:
+    rule_predictions_rule_matched = []
+    bert_predictions_rule_matched = []
+    labels_rule_matched = []
+    for x, y, z in zip(rule_predictions, bert_predictions, labels):
+        if x != 0:
+            rule_predictions_rule_matched.append(x)
+            bert_predictions_rule_matched.append(y)
+            labels_rule_matched.append(z)
+    return (
+        rule_predictions_rule_matched,
+        bert_predictions_rule_matched,
+        labels_rule_matched,
+    )
+
+
+def print_all_scores(rule_predictions: List[int], labels: List[int]) -> None:
+    print("-- all tweets --")
+    print("nr of rule_predictions: ", len([x for x in rule_predictions if x != 0]))
+    print("nr of unknown: ", len([x for x in rule_predictions if x == 0]))
+    confusion_matrix_rules = confusion_matrix(labels, rule_predictions)
+    print("confusion matrix:\n", confusion_matrix_rules)
+
+
+def print_rule_scores(
+    rule_predictions: List[int], bert_predictions: List[int], labels: List[int],
+) -> None:
+    print("-- only tweets predicted with rules --")
+    accuracy_rules = accuracy_score(labels, rule_predictions)
+    print("accuracy rules: ", accuracy_rules)
+    accuracy_bert = accuracy_score(labels, bert_predictions)
+    print("accuracy bert: ", accuracy_bert)
+    confusion_matrix_rules = confusion_matrix(labels, rule_predictions)
+    print("confusion matrix rules:\n", confusion_matrix_rules)
+    confusion_matrix_bert = confusion_matrix(labels, bert_predictions)
+    print("confusion matrix bert:\n", confusion_matrix_bert)
+
+
+def apply_rules(tweets: List[str]) -> List[int]:
+    rule_predictions_parenthesis = count_parenthesis(tweets)
+    return rule_predictions_parenthesis
+
+
+def main() -> None:
+    tweets = load_tweets("data/rules/validation_data.txt")
+    tweets_index_removed = remove_indices_from_test_tweets(tweets)
+
+    with open("data/rules/validation_labels.txt") as f:
+        labels = [int(label) for label in f]
+
+    # this are gonna be the real predictions of bert once finished
+    bert_predictions = labels
+
+    rule_predictions = apply_rules(tweets_index_removed)
+
+    (
+        rule_predictions_rule_matched,
+        bert_predictions_rule_matched,
+        labels_rule_matched,
+    ) = get_subsets_rule_based(rule_predictions, bert_predictions, labels)
+
+    print_all_scores(rule_predictions, labels)
+
+    print_rule_scores(
+        rule_predictions_rule_matched,
+        bert_predictions_rule_matched,
+        labels_rule_matched,
+    )
+
+
+if __name__ == "__main__":
+    main()
