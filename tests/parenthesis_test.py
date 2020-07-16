39ff7d67306bb3006f854e8c96d4cad74785d2f0
Vanessa Tschichold <vanessa@tschichold.com>
2020-07-16 12:17:36 +0200

    add parenthesis remove test

--- tests/parenthesis_test.py
+++ tests/parenthesis_test.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 from rules.rules import ParenthesisRule
 
 
-class ParenthesisTest(TestCase):
+class RemoveMatchingParenthesisTest(TestCase):
     def test_single_parenthesis_open(self) -> None:
         assert ParenthesisRule._remove_matching_parenthesis("(") == "("
 
@@ -41,3 +41,23 @@ class ParenthesisTest(TestCase):
         assert (
             ParenthesisRule._remove_matching_parenthesis("( hello ) ( ") == " hello  ( "
         )
+
+
+class RemoveUnmatchingParenthesisTest(TestCase):
+    def test_remove_parenthesis_closed(self) -> None:
+        assert ParenthesisRule.remove_rule_pattern_from(" ( hi ) ") == " ( hi ) "
+
+    def test_remove_parenthesis_single(self) -> None:
+        assert ParenthesisRule.remove_rule_pattern_from(" ) ") == "  "
+
+    def test_remove_parenthesis_double(self) -> None:
+        assert ParenthesisRule.remove_rule_pattern_from(" ( ( ") == "  "
+
+    def test_remove_parenthesis_closed_open(self) -> None:
+        assert ParenthesisRule.remove_rule_pattern_from(" ( hi ) ( ") == " ( hi ) "
+
+    def test_remove_parenthesis_open_closed(self) -> None:
+        assert ParenthesisRule.remove_rule_pattern_from(" ( ( hi ) ") == " ( hi ) "
+
+    def test_remove_parenthesis_closed_closed(self) -> None:
+        assert ParenthesisRule.remove_rule_pattern_from(" ( ( hi ) ") == " ( hi ) "
