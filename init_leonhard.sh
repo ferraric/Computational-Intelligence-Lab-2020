d67a4e1026aa485988ce5a2cc5057bf25879fd14
Sinan Demirci <demirci.sinan96@gmail.com>
2020-07-16 22:06:26 +0200

    move flag in requirements to pip install command

--- init_leonhard.sh
+++ init_leonhard.sh
@@ -1,6 +1,6 @@
 module load gcc/6.3.0 python_gpu/3.7.1 cuda/10.1.243 cudnn/7.6.4 eth_proxy
 source venv/bin/activate
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
 pre-commit install
 export PYTHONPATH=$PYTHONPATH:~/Computational-Intelligence-Lab-2020
 # to prevent the pre-commit hook "isort" to look through all config files in the $HOME folder 
