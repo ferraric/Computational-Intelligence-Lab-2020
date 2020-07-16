b0efcf0d78040ee3ad988b52f336365ffac92517
Vanessa Tschichold <vanessa@tschichold.com>
2020-07-16 14:38:03 +0200

    correct assertions

--- tests/parenthesis_test.py
+++ tests/parenthesis_test.py
@@ -11,40 +11,42 @@ class RemoveMatchingParenthesisTest(TestCase):
         assert ParenthesisRule(" ) ", " ( ")._remove_matching_parenthesis(")") == ")"
 
     def test_two_parenthesis_closed(self) -> None:
-        assert ParenthesisRule(" ) ", " ( ")._remove_matching_parenthesis("()") == ""
-
-    def test_two_parenthesis_nomatch(self) -> None:
-        assert ParenthesisRule(" ) ", " ( ")._remove_matching_parenthesis(")(") == ")("
+        assert ParenthesisRule(" ) ", " ( ")._remove_matching_parenthesis(" ( ) ") == ""
 
     def test_two_parenthesis_open(self) -> None:
-        assert ParenthesisRule(" ) ", " ( ")._remove_matching_parenthesis("))") == "))"
+        assert (
+            ParenthesisRule(" ) ", " ( ")._remove_matching_parenthesis(" ) ) ")
+            == " ) ) "
+        )
 
     def test_four_parenthesis_closed(self) -> None:
-        assert ParenthesisRule(" ) ", " ( ")._remove_matching_parenthesis("(())") == ""
+        assert (
+            ParenthesisRule(" ) ", " ( ")._remove_matching_parenthesis(" ( ( ) ) ")
+            == ""
+        )
 
     def test_three_parenthesis_closed(self) -> None:
-        assert ParenthesisRule(" ) ", " ( ")._remove_matching_parenthesis("())") == ")"
-
-    def test_three_parenthesis_open(self) -> None:
-        assert ParenthesisRule(" ) ", " ( ")._remove_matching_parenthesis("(()") == "("
+        assert (
+            ParenthesisRule(" ) ", " ( ")._remove_matching_parenthesis(" ( ) ) ")
+            == ") "
+        )
 
     def test_three_parenthesis_nomatch(self) -> None:
         assert (
-            ParenthesisRule(" ) ", " ( ")._remove_matching_parenthesis(")((") == ")(("
-        )
-
-    def test_four_parenthesis_nomatch(self) -> None:
-        assert (
-            ParenthesisRule(" ) ", " ( ")._remove_matching_parenthesis("))((") == "))(("
+            ParenthesisRule(" ) ", " ( ")._remove_matching_parenthesis(" ) ( ( ")
+            == " ) ( ( "
         )
 
     def test_four_parenthesis_twomatch(self) -> None:
-        assert ParenthesisRule(" ) ", " ( ")._remove_matching_parenthesis("()()") == ""
+        assert (
+            ParenthesisRule(" ) ", " ( ")._remove_matching_parenthesis(" ( ) ( ) ")
+            == ""
+        )
 
     def test_three_parenthesis_test(self) -> None:
         assert (
-            ParenthesisRule(" ) ", " ( ")._remove_matching_parenthesis("( hi ) ( ")
-            == " hi  ( "
+            ParenthesisRule(" ) ", " ( ")._remove_matching_parenthesis(" ( hi )  a ( ")
+            == "hi a ( "
         )
 
 
@@ -56,25 +58,25 @@ class RemoveUnmatchingParenthesisTest(TestCase):
         )
 
     def test_remove_parenthesis_single(self) -> None:
-        assert ParenthesisRule(" ) ", " ( ").remove_rule_pattern_from(" ) ") == "  "
+        assert ParenthesisRule(" ) ", " ( ").remove_rule_pattern_from(" ) ") == ""
 
     def test_remove_parenthesis_double(self) -> None:
-        assert ParenthesisRule(" ) ", " ( ").remove_rule_pattern_from(" ( ( ") == "  "
+        assert ParenthesisRule(" ) ", " ( ").remove_rule_pattern_from(" ( ( ") == ""
 
     def test_remove_parenthesis_closed_open(self) -> None:
         assert (
-            ParenthesisRule(" ) ", " ( ").remove_rule_pattern_from(" ( hi ) ( ")
+            ParenthesisRule(" ) ", " ( ").remove_rule_pattern_from(" ( hi )")
             == " ( hi ) "
         )
 
     def test_remove_parenthesis_open_closed(self) -> None:
         assert (
             ParenthesisRule(" ) ", " ( ").remove_rule_pattern_from(" ( ( hi ) ")
-            == " ( hi ) "
+            == "( hi ) "
         )
 
     def test_remove_parenthesis_closed_closed(self) -> None:
         assert (
             ParenthesisRule(" ) ", " ( ")._remove_rule_pattern_from(" ( ( hi ) ")
-            == " ( hi ) "
+            == "( hi ) "
         )
