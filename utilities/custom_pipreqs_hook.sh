f0a4f1c949b3bcfcf2fa345fe85bd4f3589b8690
Claudio Ferrari <ferraric@lo-login-01.leonhard.ethz.ch>
2020-07-16 17:50:19 +0200

    trying to use python 3.7.4

--- utilities/custom_pipreqs_hook.sh
+++ utilities/custom_pipreqs_hook.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 pipreqs --ignore pipreqs_ignored,.mypy_cache --savepath utilities/pipreqs.txt
-sort utilities/non_imported_requirements.txt utilities/pipreqs.txt | uniq > requirements.txt
+sort -u -t ' ' -k1,1 utilities/non_imported_requirements.txt utilities/pipreqs.txt | uniq > requirements.txt
 rm utilities/pipreqs.txt
