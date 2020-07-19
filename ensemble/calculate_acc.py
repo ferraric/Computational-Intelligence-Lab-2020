5ba90f8218a5b71e3ee801436bfa047d9cfbc8f1
Claudio Ferrari <ferraric@ethz.ch>
2020-07-19 16:08:19 +0200

    script to calculate accuracy

---
+++ ensemble/calculate_acc.py
@@ -0,0 +1,26 @@
+import argparse
+
+import numpy as np
+from sklearn.metrics import accuracy_score
+
+
+def main() -> None:
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        "-i", "--input_file", required=True,
+    )
+    argparser.add_argument(
+        "-l", "--labels", required=True,
+    )
+    args = argparser.parse_args()
+
+    predictions = np.loadtxt(
+        args.input_file, delimiter=",", dtype=np.float64, skiprows=1, usecols=(1,)
+    )
+    labels = np.loadtxt(args.labels, dtype=np.int)
+
+    print("Accuracy:", accuracy_score(labels, predictions))
+
+
+if __name__ == "__main__":
+    main()
