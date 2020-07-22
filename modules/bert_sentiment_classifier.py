be9f9bd79ce454904b90cab87b71b9fd321c2cfb
demircis <demirci.sinan96@gmail.com>
2020-07-22 16:45:44 +0200

    refactor bert to match data processor

--- modules/bert_sentiment_classifier.py
+++ modules/bert_sentiment_classifier.py
@@ -1,22 +1,14 @@
-import os
-from collections import OrderedDict
-from typing import Dict, List, Tuple, Union
+from typing import Dict, List, Union
 
 import pytorch_lightning as pl
 import torch
 from bunch import Bunch
+from modules.data_processor import DataProcessor
 from torch.nn import CrossEntropyLoss
 from torch.optim import Adam
 from torch.optim.optimizer import Optimizer
-from torch.utils.data import DataLoader, Subset, TensorDataset, random_split
+from torch.utils.data import DataLoader
 from transformers import BertForSequenceClassification, BertTokenizerFast
-from utilities.data_loading import (
-    generate_bootstrap_dataset,
-    load_tweets,
-    remove_indices_from_test_tweets,
-    save_labels,
-    save_tweets_in_test_format,
-)
 
 
 class BertSentimentClassifier(pl.LightningModule):
@@ -26,101 +18,14 @@ class BertSentimentClassifier(pl.LightningModule):
         self.model = BertForSequenceClassification.from_pretrained(
             config.pretrained_model
         )
-        self.tokenizer = BertTokenizerFast.from_pretrained(self.config.pretrained_model)
+        tokenizer = BertTokenizerFast.from_pretrained(self.config.pretrained_model)
+        self.data_processor = DataProcessor(
+            config, self.logger, tokenizer, self.testing
+        )
         self.loss = CrossEntropyLoss()
 
-    def _load_tweets(self, path: str) -> List[str]:
-        loaded_tweets = load_tweets(path)
-        self.logger.experiment.log_other(
-            key="n_tweets_from:" + path, value=len(loaded_tweets)
-        )
-        return loaded_tweets
-
-    def _load_unique_tweets(self, path: str) -> List[str]:
-        def _remove_duplicate_tweets(tweets: List[str]) -> List[str]:
-            return list(OrderedDict.fromkeys(tweets).keys())
-
-        unique_tweets = _remove_duplicate_tweets(self._load_tweets(path))
-        self.logger.experiment.log_other(
-            key="n_unique_tweets_from:" + path, value=len(unique_tweets)
-        )
-        return unique_tweets
-
-    def _generate_labels(
-        self, n_negative_samples: int, n_positive_samples: int
-    ) -> torch.Tensor:
-        return torch.cat(
-            (
-                torch.zeros(n_negative_samples, dtype=torch.int64),
-                torch.ones(n_positive_samples, dtype=torch.int64),
-            )
-        )
-
-    def _tokenize_tweets(
-        self, tokenizer: BertTokenizerFast, tweets: List[str]
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-        tokenized_input = tokenizer.batch_encode_plus(
-            tweets,
-            max_length=self.config.max_tokens_per_tweet,
-            pad_to_max_length=True,
-            return_token_type_ids=False,
-            return_tensors="pt",
-        )
-        return tokenized_input["input_ids"], tokenized_input["attention_mask"]
-
-    def _train_validation_split(
-        self, validation_size: float, data: TensorDataset, random_seed: int
-    ) -> List[Subset]:
-        assert 0 <= validation_size and validation_size <= 1
-
-        n_validation_samples = int(validation_size * len(data))
-        n_train_samples = len(data) - n_validation_samples
-        random_state_before_split = torch.get_rng_state()
-        torch.manual_seed(random_seed)
-        [train_data, validation_data] = random_split(
-            data, [n_train_samples, n_validation_samples]
-        )
-        torch.set_rng_state(random_state_before_split)
-        return [train_data, validation_data]
-
     def prepare_data(self) -> None:
-        negative_tweets = self._load_unique_tweets(self.config.negative_tweets_path)
-        positive_tweets = self._load_unique_tweets(self.config.positive_tweets_path)
-        all_tweets = negative_tweets + positive_tweets
-        labels = self._generate_labels(len(negative_tweets), len(positive_tweets))
-
-        train_token_ids, train_attention_mask = self._tokenize_tweets(
-            self.tokenizer, all_tweets
-        )
-
-        self.train_data, self.validation_data = self._train_validation_split(
-            self.config.validation_size,
-            TensorDataset(train_token_ids, train_attention_mask, labels),
-            self.config.validation_split_random_seed,
-        )
-
-        if not self.testing:
-            validation_indices = list(self.validation_data.indices)
-            validation_tweets = [all_tweets[i] for i in validation_indices]
-            validation_labels = labels[validation_indices]
-            save_tweets_in_test_format(
-                validation_tweets,
-                os.path.join(self.config.model_save_path, "validation_data.txt"),
-            )
-            save_labels(
-                validation_labels,
-                os.path.join(self.config.model_save_path, "validation_labels.txt"),
-            )
-
-        if self.config.do_bootstrap_sampling:
-            self.train_data = generate_bootstrap_dataset(self.train_data)
-
-        test_tweets = self._load_tweets(self.config.test_tweets_path)
-        test_tweets_index_removed = remove_indices_from_test_tweets(test_tweets)
-        test_token_ids, test_attention_mask = self._tokenize_tweets(
-            self.tokenizer, test_tweets_index_removed
-        )
-        self.test_data = TensorDataset(test_token_ids, test_attention_mask)
+        self.data_processor.prepare_data()
 
     def forward(
         self, token_ids: torch.Tensor, attention_mask: torch.Tensor
