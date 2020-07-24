1ea48bff7db8b1023ab05b45eb05a89705416bf3
demircis <demirci.sinan96@gmail.com>
2020-07-24 13:36:16 +0200

    fix imports

--- rules/main.py
+++ rules/main.py
@@ -3,7 +3,7 @@ from typing import List
 
 import numpy as np
 import pandas as pd
-from rule_classifier import RuleClassifier
+from rules.rule_classifier import RuleClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 from utilities.data_loading import (
     load_tweets,
