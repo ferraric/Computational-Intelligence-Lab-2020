e14bd9b9e8fd67e51e95026c7a25cd8f413fb388
demircis <demirci.sinan96@gmail.com>
2020-07-26 16:17:29 +0200

    remove unrelated rules

--- rule/rules.py
+++ rule/rules.py
@@ -43,27 +43,6 @@ class PositiveNegativeRule(Rule):
         return self.remove_double_whitespaces(tweet_without_rule_pattern)
 
 
-class NegativeRule(Rule):
-    def __init__(self, negative_pattern: str):
-        self.negative_pattern = negative_pattern
-
-    def apply(self, tweet: str) -> int:
-        if self.negative_pattern in tweet:
-            return -1
-        else:
-            return 0
-
-    def remove_rule_pattern_from(self, tweet: str) -> str:
-        if self.apply(tweet) == 0:
-            tweet_without_rule_pattern = tweet
-        elif self.apply(tweet) == -1:
-            tweet_without_rule_pattern = tweet.replace(self.negative_pattern, "")
-        else:
-            raise ValueError("rule application returned unexpected value")
-
-        return self.remove_double_whitespaces(tweet_without_rule_pattern)
-
-
 class ParenthesisRule(PositiveNegativeRule):
     def apply(self, tweet: str) -> int:
         return super().apply(self.remove_matching_parenthesis(tweet))
@@ -116,25 +95,3 @@ class ParenthesisRule(PositiveNegativeRule):
             if (char == self.positive_pattern) or (char == self.negative_pattern):
                 indices.append(i)
         return indices
-
-
-class HappySadHashtagRule(PositiveNegativeRule):
-    def remove_rule_pattern_from(self, tweet: str) -> str:
-        if self.apply(tweet) == 0:
-            tweet_without_rule_pattern = tweet
-        elif self.apply(tweet) == 1:
-            tweet_without_rule_pattern = " ".join(
-                word
-                for word in tweet.split(" ")
-                if not word.startswith(self.positive_pattern)
-            )
-        elif self.apply(tweet) == -1:
-            tweet_without_rule_pattern = " ".join(
-                word
-                for word in tweet.split(" ")
-                if not word.startswith(self.negative_pattern)
-            )
-        else:
-            raise ValueError("rule application returned unexpected value")
-
-        return self.remove_double_whitespaces(tweet_without_rule_pattern)
