5d84c5426714278a49bb922303d2fe9ca0ba2370
Claudio Ferrari <ferraric@ethz.ch>
2020-07-29 08:28:48 +0200

    removed save option

--- rule/main.py
+++ rule/main.py
@@ -3,10 +3,7 @@ from typing import List
 
 import numpy as np
 import pandas as pd
-from data_processing.data_loading_and_storing import (
-    load_test_tweets,
-    save_tweets_in_test_format,
-)
+from data_processing.data_loading_and_storing import load_test_tweets
 from rule.rule_classifier import RuleClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 
@@ -27,11 +24,6 @@ def get_args() -> argparse.Namespace:
     )
     argparser.add_argument(
         "-b", "--bert_predictions_path", help="Path to the BERT predictions (csv)",
-    )
-    argparser.add_argument(
-        "-s",
-        "--save_path",
-        help="Path where to save the tweets without the rule patterns",
     )
     return argparser.parse_args()
 
@@ -55,66 +47,53 @@ def main() -> None:
 
     rule_classifier = RuleClassifier()
 
-    if args.save_path is not None:
-        tweets_without_rule_patterns = rule_classifier.remove_rule_patterns_from(tweets)
-        save_tweets_in_test_format(tweets_without_rule_patterns, args.save_path)
-        print("tweets saved")
+    labels = np.loadtxt(args.validation_labels_path, dtype=np.int64)
 
-    elif args.bert_predictions_path is not None:
-        labels = np.loadtxt(args.validation_labels_path, dtype=np.int64)
+    bert_predictions = np.loadtxt(
+        args.bert_predictions_path,
+        delimiter=",",
+        dtype=np.int64,
+        skiprows=1,
+        usecols=(1,),
+    )
 
-        bert_predictions = np.loadtxt(
-            args.bert_predictions_path,
-            delimiter=",",
-            dtype=np.int64,
-            skiprows=1,
-            usecols=(1,),
-        )
+    rule_predictions = rule_classifier.predict(tweets)
 
-        rule_predictions = rule_classifier.predict(tweets)
+    print_confusion_matrix(
+        labels,
+        rule_predictions,
+        label_names=["negative", "unknown", "positive"],
+        title="rule based",
+    )
+    print_confusion_matrix(
+        labels, bert_predictions, label_names=["negative", "positive"], title="bert"
+    )
 
-        print_confusion_matrix(
-            labels,
-            rule_predictions,
-            label_names=["negative", "unknown", "positive"],
-            title="rule based",
-        )
-        print_confusion_matrix(
-            labels, bert_predictions, label_names=["negative", "positive"], title="bert"
-        )
+    rule_predictions_rule_matched = rule_predictions[rule_predictions != 0]
+    bert_predictions_rule_matched = bert_predictions[rule_predictions != 0]
+    labels_rule_matched = labels[rule_predictions != 0]
 
-        rule_predictions_rule_matched = rule_predictions[rule_predictions != 0]
-        bert_predictions_rule_matched = bert_predictions[rule_predictions != 0]
-        labels_rule_matched = labels[rule_predictions != 0]
+    print(
+        "Percentage of rule matches:",
+        len(rule_predictions_rule_matched) / len(rule_predictions),
+    )
+    accuracy_rules = accuracy_score(labels_rule_matched, rule_predictions_rule_matched)
+    print("accuracy rules: ", accuracy_rules)
+    accuracy_bert = accuracy_score(labels_rule_matched, bert_predictions_rule_matched)
+    print("accuracy bert: ", accuracy_bert)
 
-        print(
-            "Percentage of rule matches:",
-            len(rule_predictions_rule_matched) / len(rule_predictions),
-        )
-        accuracy_rules = accuracy_score(
-            labels_rule_matched, rule_predictions_rule_matched
-        )
-        print("accuracy rules: ", accuracy_rules)
-        accuracy_bert = accuracy_score(
-            labels_rule_matched, bert_predictions_rule_matched
-        )
-        print("accuracy bert: ", accuracy_bert)
-
-        print_confusion_matrix(
-            labels_rule_matched,
-            rule_predictions_rule_matched,
-            label_names=["negative", "positive"],
-            title="rule based on rule match",
-        )
-        print_confusion_matrix(
-            labels_rule_matched,
-            bert_predictions_rule_matched,
-            label_names=["negative", "positive"],
-            title="bert on rule match",
-        )
-
-    else:
-        raise ValueError("-b and -s flag not specified")
+    print_confusion_matrix(
+        labels_rule_matched,
+        rule_predictions_rule_matched,
+        label_names=["negative", "positive"],
+        title="rule based on rule match",
+    )
+    print_confusion_matrix(
+        labels_rule_matched,
+        bert_predictions_rule_matched,
+        label_names=["negative", "positive"],
+        title="bert on rule match",
+    )
 
 
 if __name__ == "__main__":
