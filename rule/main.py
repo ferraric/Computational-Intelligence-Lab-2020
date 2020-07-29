024eb8c0588061bc961a562e5f6f7ad996bc8bd0
Vanessa Tschichold <vanessa@tschichold.com>
2020-07-29 05:07:00 +0200

    clean printing

--- rule/main.py
+++ rule/main.py
@@ -126,30 +126,32 @@ def main() -> None:
             labels_rule_matched = labels[rule_predictions != 0]
 
             print(
-                "Percentage of rule matches:",
+                "percentage of rule matches:",
                 len(rule_predictions_rule_matched) / len(rule_predictions),
             )
             accuracy_rules = accuracy_score(
                 labels_rule_matched, rule_predictions_rule_matched
             )
-            print("accuracy rules: ", accuracy_rules)
+            print("accuracy rule: ", accuracy_rules)
             accuracy_bert = accuracy_score(
                 labels_rule_matched, bert_predictions_rule_matched
             )
-            print("accuracy bert: ", accuracy_bert)
 
             print_confusion_matrix(
                 labels_rule_matched,
                 rule_predictions_rule_matched,
                 label_names=["negative", "positive"],
-                title="rule based on rule match",
+                title="rules on rule subset",
             )
-            print_confusion_matrix(
-                labels_rule_matched,
-                bert_predictions_rule_matched,
-                label_names=["negative", "positive"],
-                title="bert on rule match",
-            )
+
+        print("accuracy bert: ", accuracy_bert)
+
+        print_confusion_matrix(
+            labels_rule_matched,
+            bert_predictions_rule_matched,
+            label_names=["negative", "positive"],
+            title="bert on rule subset",
+        )
 
     else:
         raise ValueError("-b and -s flag not specified")
